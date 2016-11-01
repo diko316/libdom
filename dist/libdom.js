baa2c0bc542fbacb394fe8363fe41ec5cae0152a
Diko Tech Slave <diko316@gmail.com>
2016-11-01 15:20:43 +0800

    added feature not to throw browser errors if run in nodejs.

--- dist/libdom.js
+++ dist/libdom.js
@@ -24,67 +24,65 @@
         (function(global) {
             "use strict";
             var CORE = __webpack_require__(2), detect = __webpack_require__(11), EXPORTS = {
+                env: CORE.env,
                 info: detect
             };
             var css, event, dimension, selection;
-            function notBrowser() {
-                throw new Error("Unable to proceed, not running in a browser.");
-            }
             function applyIf(api, moduleObject, access) {
-                var hasOwn = CORE.contains, handler = detect ? false : notBrowser;
+                var hasOwn = CORE.contains;
                 var name;
                 for (name in access) {
                     if (hasOwn(access, name)) {
-                        api[name] = handler || moduleObject[access[name]];
+                        api[name] = moduleObject[access[name]];
                     }
                 }
             }
-            applyIf(EXPORTS, __webpack_require__(18), {
-                is: "is",
-                isView: "isView",
-                contains: "contains",
-                select: "select",
-                eachNodePreorder: "eachPreorder",
-                eachNodePostorder: "eachPostorder",
-                eachNodeLevelorder: "eachLevel",
-                add: "add",
-                remove: "remove"
-            });
-            applyIf(EXPORTS, css = __webpack_require__(20), {
-                addClass: "add",
-                removeClass: "remove",
-                computedStyle: "computedStyle",
-                stylize: "style"
-            });
-            applyIf(EXPORTS, event = __webpack_require__(28), {
-                on: "on",
-                un: "un",
-                purge: "purge",
-                dispatch: "fire"
-            });
-            applyIf(EXPORTS, dimension = __webpack_require__(29), {
-                offset: "offset",
-                size: "size",
-                box: "box",
-                scroll: "scroll",
-                screen: "screen"
-            });
-            applyIf(EXPORTS, selection = __webpack_require__(30), {
-                highlight: "select",
-                noHighlight: "unselectable",
-                clearHighlight: "clear"
-            });
-            applyIf(EXPORTS, __webpack_require__(21), {
-                parseColor: "parse",
-                formatColor: "stringify"
-            });
-            applyIf(EXPORTS, __webpack_require__(31), {
-                eachDisplacement: "each",
-                animateStyle: "style"
-            });
             if (detect) {
+                applyIf(EXPORTS, __webpack_require__(18), {
+                    is: "is",
+                    isView: "isView",
+                    contains: "contains",
+                    select: "select",
+                    eachNodePreorder: "eachPreorder",
+                    eachNodePostorder: "eachPostorder",
+                    eachNodeLevelorder: "eachLevel",
+                    add: "add",
+                    remove: "remove"
+                });
+                applyIf(EXPORTS, css = __webpack_require__(20), {
+                    addClass: "add",
+                    removeClass: "remove",
+                    computedStyle: "computedStyle",
+                    stylize: "style"
+                });
+                applyIf(EXPORTS, event = __webpack_require__(28), {
+                    on: "on",
+                    un: "un",
+                    purge: "purge",
+                    dispatch: "fire"
+                });
+                applyIf(EXPORTS, dimension = __webpack_require__(29), {
+                    offset: "offset",
+                    size: "size",
+                    box: "box",
+                    scroll: "scroll",
+                    screen: "screen"
+                });
+                applyIf(EXPORTS, selection = __webpack_require__(30), {
+                    highlight: "select",
+                    noHighlight: "unselectable",
+                    clearHighlight: "clear"
+                });
+                applyIf(EXPORTS, __webpack_require__(21), {
+                    parseColor: "parse",
+                    formatColor: "stringify"
+                });
+                applyIf(EXPORTS, __webpack_require__(31), {
+                    eachDisplacement: "each",
+                    animateStyle: "style"
+                });
                 css.chain = event.chain = dimension.chain = selection.chain = EXPORTS;
-            }
+            } else {}
             module.exports = global.libdom = EXPORTS["default"] = EXPORTS;
         }).call(exports, function() {
             return this;
--- src/index.js
+++ src/index.js
@@ -3,105 +3,103 @@
 var CORE = require('libcore'),
     detect = require("./lib/detect.js"),
     EXPORTS = {
+        env: CORE.env,
         info: detect
     };
 var css, event, dimension, selection;
 
-function notBrowser() {
-    throw new Error("Unable to proceed, not running in a browser.");
-}
-
 
 function applyIf(api, moduleObject, access) {
-    var hasOwn = CORE.contains,
-        handler = detect ? false : notBrowser;
+    var hasOwn = CORE.contains;
     var name;
     
     for (name in access) {
         if (hasOwn(access, name)) {
-            api[name] = handler || moduleObject[access[name]];
+            api[name] = moduleObject[access[name]];
         }
     }
 }
 
 
-// dom structure
-applyIf(EXPORTS,
-        require("./lib/dom.js"),
-        {
-            'is': 'is',
-            'isView': 'isView',
-            'contains': 'contains',
-            
-            'select': 'select',
-            
-            'eachNodePreorder': 'eachPreorder',
-            'eachNodePostorder': 'eachPostorder',
-            'eachNodeLevelorder': 'eachLevel',
-            
-            'add': 'add',
-            'remove': 'remove'
-        });
-
-applyIf(EXPORTS,
-        css = require("./lib/css.js"),
-        {
-            'addClass': 'add',
-            'removeClass': 'remove',
-            'computedStyle': 'computedStyle',
-            'stylize': 'style'
-        });
-
-
-applyIf(EXPORTS,
-        event = require("./lib/event.js"),
-        {
-            'on': 'on',
-            'un': 'un',
-            'purge': 'purge',
-            'dispatch': 'fire'
-        });
-
-applyIf(EXPORTS,
-        dimension = require("./lib/dimension.js"),
-        {
-            'offset': 'offset',
-            'size': 'size',
-            'box': 'box',
-            'scroll': 'scroll',
-            'screen': 'screen'
-        });
-
-applyIf(EXPORTS,
-        selection = require("./lib/selection.js"),
-        {
-            'highlight': 'select',
-            'noHighlight': 'unselectable',
-            'clearHighlight': 'clear'
-        });
-
-applyIf(EXPORTS,
-        require("./lib/color.js"),
-        {
-            'parseColor': 'parse',
-            'formatColor': 'stringify'
-        });
-
-applyIf(EXPORTS,
-        require("./lib/animation.js"),
-        {
-            'eachDisplacement': 'each',
-            'animateStyle': 'style'
-        });
-
 if (detect) {
+
+    // dom structure
+    applyIf(EXPORTS,
+            require("./lib/dom.js"),
+            {
+                'is': 'is',
+                'isView': 'isView',
+                'contains': 'contains',
+                
+                'select': 'select',
+                
+                'eachNodePreorder': 'eachPreorder',
+                'eachNodePostorder': 'eachPostorder',
+                'eachNodeLevelorder': 'eachLevel',
+                
+                'add': 'add',
+                'remove': 'remove'
+            });
+    
+    applyIf(EXPORTS,
+            css = require("./lib/css.js"),
+            {
+                'addClass': 'add',
+                'removeClass': 'remove',
+                'computedStyle': 'computedStyle',
+                'stylize': 'style'
+            });
+    
+    
+    applyIf(EXPORTS,
+            event = require("./lib/event.js"),
+            {
+                'on': 'on',
+                'un': 'un',
+                'purge': 'purge',
+                'dispatch': 'fire'
+            });
+    
+    applyIf(EXPORTS,
+            dimension = require("./lib/dimension.js"),
+            {
+                'offset': 'offset',
+                'size': 'size',
+                'box': 'box',
+                'scroll': 'scroll',
+                'screen': 'screen'
+            });
+    
+    applyIf(EXPORTS,
+            selection = require("./lib/selection.js"),
+            {
+                'highlight': 'select',
+                'noHighlight': 'unselectable',
+                'clearHighlight': 'clear'
+            });
+    
+    applyIf(EXPORTS,
+            require("./lib/color.js"),
+            {
+                'parseColor': 'parse',
+                'formatColor': 'stringify'
+            });
+    
+    applyIf(EXPORTS,
+            require("./lib/animation.js"),
+            {
+                'eachDisplacement': 'each',
+                'animateStyle': 'style'
+            });
+    
     css.chain =
         event.chain = 
         dimension.chain =
         selection.chain = EXPORTS;
 }
-
-
+else {
+    //throw new Error("Unable to proceed, not running in a browser.");
+}
 
 module.exports = global.libdom = EXPORTS['default'] = EXPORTS;
 
